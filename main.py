3401e2799a82bf54fdc4f699e807d9145395bce4
Jaroslaw Drzezdzon <jd@vlo.gda.pl>
2020-06-20 09:24:14 +0200

    Dodanie funkcji isPrime

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+    Zadanie 4 z matury poziom rozszerzony - nowa matura, czerwiec 2020
+    Autor: Jarosław Drzeżdżon
+    Data: 19 czerwca 2020 r.
+'''
+from zadanie1 import *
+
+def zadanie4_1():
+    print(isPrime(13))
+    print(isPrime(20))
+
+def main():
+    zadanie4_1()
+
+if __name__ == "__main__":
+    main()
