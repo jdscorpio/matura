bb512ae1fe07e4b24e2234293147679ba5a14b2c
Jaroslaw Drzezdzon <jd@vlo.gda.pl>
2020-06-20 10:28:06 +0200

    Dodanie funkcji LCF

--- zadanie2.py
+++ zadanie2.py
@@ -10,3 +10,16 @@ def indexMax(tab:list)->int:
         if tab[i] == max(tab):
             return i
 
+def LCF(txt:str):
+# Najdłuższy spójny fragment
+    tab = [0 for x in range(len(txt))]
+    tab[0] = 1
+    for i in range(1,len(txt)):
+        if txt[i-1] == txt[i]:
+            tab[i] = tab[i-1] + 1
+        else:
+            tab[i] = 1
+    m = max(tab)
+    text = txt[indexMax(tab)]*m 
+
+    return text,m 
