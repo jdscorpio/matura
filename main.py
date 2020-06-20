6b3a443a28d08caee758c549beea423c165e91b4
Jaroslaw Drzezdzon <jd@vlo.gda.pl>
2020-06-20 10:00:20 +0200

    Dodanie funkcji zadanie4_1

--- main.py
+++ main.py
@@ -5,13 +5,20 @@
 '''
 from zadanie1 import *
 
-def zadanie4_1():
-    print(goldbach(20))
-    print(goldbach(24))
-    print(goldbach(6))
+def zadanie4_1(filename_in:str,filename_ou:str):
+    output = open(filename_ou,"w")
+    output.write("4.1:\n")   
+
+    with open(filename_in) as file:
+        for line in file:
+            n,txt = line.split()
+            temp = goldbach(int(n))
+            if temp[0]!=0:
+                output.write(f"{n} {temp[0]} {temp[1]}\n") 
+    
 
 def main():
-    zadanie4_1()
+    zadanie4_1("przyklad.txt","odp.txt")
 
 if __name__ == "__main__":
     main()
